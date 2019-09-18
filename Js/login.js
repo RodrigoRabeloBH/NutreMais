8c109a4f1b13151c3035754a7db51c5768d54f98
Rodrigo <47299516+RodrigoRabeloBH@users.noreply.github.com>
2019-09-18 10:58:50 -0300

    Add files via upload

--- Js/login.js
+++ Js/login.js
@@ -1,30 +1,21 @@
 
 const obj = JSON.parse(localStorage.getItem('key'));
 
-console.log(obj);
-
 const btn = document.querySelector('.btn').addEventListener('click', Login);
 
 function Login(e) {
 
     const pass = document.querySelector('#Password').value;
     const mail = document.querySelector('#email').value;
-   
-    console.log(pass);
-    console.log(mail);
-
-    if (obj === null) {
-        return false;
-    } else {
+     
         obj.forEach(element => {
             if (pass == element.password && element.email == mail) {
 			console.log(element.password, element.email);	
                 window.location.assign("https://nutrimais.netlify.com/data.html");
+
             } else {
-                // alert("Email ou senha não confere");
-			// console.log(element.password, element.email);
+                 alert("Email ou senha não confere");
+			
             }
-        });
+        });    
     }
-    e.preventDefault();
-}
