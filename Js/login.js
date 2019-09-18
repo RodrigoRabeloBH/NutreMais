bc7ab04c5a18017e3d9ec03b2dbb107f9fd72d7f
Rodrigo <47299516+RodrigoRabeloBH@users.noreply.github.com>
2019-09-18 11:01:42 -0300

    v2

--- Js/login.js
+++ Js/login.js
@@ -9,10 +9,10 @@ function Login(e) {
     const mail = document.querySelector('#email').value;
      
         obj.forEach(element => {
-            if (pass == element.password && element.email == mail) {			
+            if (pass ===  element.password && element.email === mail) {			
                 window.location.assign("https://nutrimais.netlify.com/data.html");
             } else {
-                 alert("Email ou senha não confere");			
+                 // alert("Email ou senha não confere");			
             }
         });    
     }
