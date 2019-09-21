5204972ffcd837118409e9fdbc29553e43704835
Wfmsa <aluno@MSHLI12418.animaedu.intranet>
2019-09-20 22:08:38 -0300

    Alterei o diretorio do Js/login.Antes tinha o site completo e não preciava

--- Js/login.js
+++ Js/login.js
@@ -17,7 +17,7 @@ function Login(e) {
     } else {
         base.forEach(element => {
             if (pass === element.password && element.email === mail) {
-                window.location.assign("https://nutrimais.netlify.com/data.html");
+                window.location.assign("data.html");
             } else {
                 alert("Email ou senha não confere");
             }
